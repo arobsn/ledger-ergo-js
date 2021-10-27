e6dc3b239f5c750ca2bd4f4333130c7477a85199
anon-br <sigmanon@protonmail.com>
2021-10-27 12:13:54 -0300

    Add some initial test

--- tests/library.test.ts
+++
@@ -1,7 +0,0 @@
-import sampleLibrary from "../src/index";
-
-describe("Base tests", () => {
-  it("should create Library", () => {
-    expect(sampleLibrary()).toBe("Hello World!");
-  });
-});
---
+++ tests/utils.test.ts
@@ -0,0 +1,11 @@
+import { chunkBy } from "../src/interactions/common/utils";
+
+describe("Utils test", () => {
+  it("should chunk buffers", () => {
+    const buffer = Buffer.alloc(11);
+    const [first, last] = chunkBy(buffer, [5, 6]);
+
+    expect(first.length).toBe(5);
+    expect(last.length).toBe(6);
+  });
+});
