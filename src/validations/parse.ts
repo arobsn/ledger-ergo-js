147684c3f910a0a31866ad2d85fd816edeeef3fe
anon-br <sigmanon@protonmail.com>
2021-11-04 19:44:07 -0300

    add chuckBy validations

--- src/validations/parse.ts
+++ src/validations/parse.ts
@@ -17,3 +17,11 @@ export function isValidErgoPath(path: number[]): boolean {
   const [ergoPurpose, ergoCoinType] = bip32Path.fromString("m/44'/429'").toPathArray();
   return pathPurpose === ergoPurpose && pathCoinType === ergoCoinType;
 }
+
+export const isString = (data: unknown) => typeof data === "string";
+
+export const isInteger = (data: unknown) => Number.isInteger(data);
+
+export const isArray = (data: unknown) => Array.isArray(data);
+
+export const isBuffer = (data: unknown) => Buffer.isBuffer(data);
