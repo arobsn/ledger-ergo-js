d3539dfd87ede531201e4f14c9b22028c06924b1
anon-br <sigmanon@protonmail.com>
2021-11-04 20:56:53 -0300

    add isUint64String

--- src/validations/parse.ts
+++ src/validations/parse.ts
@@ -1,5 +1,8 @@
 import bip32Path from "bip32-path";
 
+export const MIN_UINT_64_STR = "0";
+export const MAX_UINT_64_STR = "18446744073709551615";
+
 export function isValidBip32Path(path: number[] | string): boolean {
   if (typeof path == "string") {
     return bip32Path.validateString(path, true);
@@ -25,3 +28,20 @@ export const isInteger = (data: unknown) => Number.isInteger(data);
 export const isArray = (data: unknown) => Array.isArray(data);
 
 export const isBuffer = (data: unknown) => Buffer.isBuffer(data);
+
+export function isUint64String(data: string): boolean {
+  return (
+    isString(data) &&
+    /^[0-9]*$/.test(data) &&
+    // Length checks
+    data.length > 0 &&
+    data.length <= MAX_UINT_64_STR.length &&
+    // Leading zeros
+    (data.length === 1 || data[0] !== "0") &&
+    // less or equal than max value
+    // Note: this is string comparison!
+    (data.length < MAX_UINT_64_STR.length || data <= MAX_UINT_64_STR) &&
+    // Note: this is string comparison!
+    (data.length > MIN_UINT_64_STR.length || data >= MIN_UINT_64_STR)
+  );
+}
